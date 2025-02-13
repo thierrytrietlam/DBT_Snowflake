575a6eb12510b2456f6d1856294dafcb82d9bd2d
Thierry Lam <thierrylam.aifr@gmail.com>
2025-02-13 01:16:28 +0100

    incremental

--- models/staging/order/stg_tpch_orders.sql
+++ models/staging/order/stg_tpch_orders.sql
@@ -1,8 +1,19 @@
+-- {{ config(materialized='incremental') }}
+    -- materialized='ephemeral'
+    -- partition_by = ['o_orderdate'],
+    -- cluster_by = ['O_ORDERKEY'], -- faster filtering
+    -- unique_key='o_orderkey'
 select
     o_orderkey as order_key,
     o_custkey as customer_key,
     o_orderstatus as status_code,
     o_totalprice as total_price,
-    o_orderdate as order_date
+    o_orderdate as order_date,
+    -- DATEADD(DAY, -10, CURRENT_TIMESTAMP()) as updated_at
+
 from
-    {{ source('tpch', 'orders') }}
+    {{ source('tpch', 'orders') }} AS o
+WHERE o_orderkey IS NOT NULL
+{% if is_incremental() %}
+  AND o.o_orderdate > (SELECT MAX(order_date) FROM {{ this }})
+{% endif %}
--- models/staging/shipmode/stg_shipmode.sql
+++ models/staging/shipmode/stg_shipmode.sql
@@ -1,5 +1,14 @@
+{{ config(
+    materialized='incremental',
+    unique_key='SM_SHIP_MODE_SK'
+) }}
+    -- materialized='ephemeral'
+
 select
     sm_code,
     array_agg(distinct sm_carrier) as sm_carrier_array
 from {{source('shipmode', 'ship_mode')}}
 group by 1
+{% if is_incremental() %}
+  WHERE updated_at > (SELECT CURRENT_DATE()-1000 FROM {{ this }})
+{% endif %}
